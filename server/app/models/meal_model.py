5d2c7b29470a1c59bb6cb5e27b28678c8b5bbd46
Nhatkhoi <nhatkhoi1905@gmail.com>
2026-06-16 03:48:04 +0700

    edit meal_model.py

--- server/app/models/meal_model.py
+++ server/app/models/meal_model.py
@@ -1,15 +1,17 @@
+import os
+
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017")
+client = MongoClient(os.getenv("MONGO_URI"))
 db = client["meal_planner_db"]
 meals_collection = db["meals"]
 
-# def get_meals_by_type(meal_type):
-#     return list(meals_collection.find({"type": meal_type}, {
-#         "_id": 0,  # loại bỏ ObjectId
-#         "id": 1,
-#         "name": 1,
-#         "calories": 1,
-#         "price": 1,
-#         "image": 1
-#     }))
+def get_meals_by_type(meal_type):
+    return list(meals_collection.find({"type": meal_type}, {
+        "_id": 0,  # loại bỏ ObjectId
+        "id": 1,
+        "name": 1,
+        "calories": 1,
+        "price": 1,
+        "image": 1
+    }))
