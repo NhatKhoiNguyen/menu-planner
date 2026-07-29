5237f4f29c507da5b62a8677f60bdc32fb4087ab
Nhatkhoi <nhatkhoi1905@gmail.com>
2026-07-29 18:12:12 +0700

    fix pagination 2

--- client/src/components/MealManagement.jsx
+++ client/src/components/MealManagement.jsx
@@ -46,8 +46,7 @@ export default function MealManagement() {
       const data = await res.json();
 
       setMeals(data.meals);
-      console.log("total = " + data.totalPages);
-      setTotalPages(data.totalPages);
+      setTotalPages(data.pagination.totalPages);
     } catch (err) {
       console.error(err);
     }
