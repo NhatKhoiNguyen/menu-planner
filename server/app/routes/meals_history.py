b42b8e46c3352f98d7f20c6415dbf40575eb9d0a
Nhatkhoi <nhatkhoi1905@gmail.com>
2026-07-30 22:30:49 +0700

    fix meal history route 1

--- server/app/routes/meals_history.py
+++ server/app/routes/meals_history.py
@@ -86,25 +86,20 @@ def save_meal_history(current_user):
 #         result.append(item)
 #     return jsonify(result), 200
 
-
 @meals_history_bp.route("/list", methods=["GET"])
-@login_required
-def get_meal_history(current_user):
-    print("STEP 1")
+def get_meal_history():
+    return jsonify({"ok": True})
 
-    user_id = str(current_user["_id"])
+# @meals_history_bp.route("/list", methods=["GET"])
+# @login_required
+# def get_meal_history(current_user):
+#     print("1")
 
-    histories = list(
-        current_app.db.meal_histories.find({"user_id": user_id}).sort("date", -1)
-    )
+#     history = MealHistory.find_by_user(current_user["_id"])
 
-    print("Histories:", len(histories))
+#     print("2")
 
-    return jsonify({
-        "ok": True,
-        "count": len(histories)
-    })
-
+#     return jsonify({"ok": True})
 
 # @meals_history_bp.route("/list", methods=["GET"])
 # @login_required
